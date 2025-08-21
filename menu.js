b104d7903ed13c6926e06fcf4376a77cf61cd610
Rafaela Brietzke <rlbrietzke@gmail.com>
2025-08-21 08:49:14 -0300

    inclusão do tema light

--- menu.js
+++ menu.js
@@ -13,3 +13,22 @@ menu.addEventListener ('click', ()=> {
 overlay.addEventListener ('click', ()=> {
     menu.classList.remove('abrir-menu')
 }) 
+
+
+// mudando o tema 
+
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+
+function changeTheme(){
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+
+    if(currentTheme === "dark")rootHtml.setAttribute("data-theme", "light")
+    else rootHtml.setAttribute("data-theme", "dark")
+
+    toggleTheme.classList.toggle("bi-sun")
+    toggleTheme.classList.toggle("bi-moon-stars")
+}
+
+toggleTheme.addEventListener("click", changeTheme);
